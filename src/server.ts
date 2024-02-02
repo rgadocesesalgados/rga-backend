3923d7bdcafce5355646d6846aff9bd672da934d
devHonorio <honorio.dev@gmail.com>
2024-02-01 23:47:10 -0300

    Tratamento de erros

--- src/server.ts
+++ src/server.ts
@@ -1,4 +1,5 @@
-import express from 'express'
+import 'express-async-errors'
+import express, { NextFunction, Request, Response } from 'express'
 import { routes } from './routes'
 
 const app = express()
@@ -7,6 +8,17 @@ app.use(express.json())
 
 app.use(routes)
 
+app.use((err: Error, req: Request, res: Response, next: NextFunction) => {
+  if (err instanceof Error) {
+    return res.status(400).json({
+      error: err.message,
+    })
+  }
+  return res.status(500).json({
+    status: 'error',
+    message: 'Internal server error',
+  })
+})
 app.listen(3000, () => {
   console.log('Server started on port http://localhost:3000')
 })
