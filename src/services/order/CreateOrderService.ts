b90c66bb658991ecae01356adcf01fde5cbb0842
devHonorio <honorio.dev@gmail.com>
2024-02-06 00:19:39 -0300

    Remove interface de CreateOrder

--- src/services/order/CreateOrderService.ts
+++ src/services/order/CreateOrderService.ts
@@ -1,14 +1,4 @@
 import { prismaClient } from '../../prisma'
-import { AddressProps } from '../client/CreateClientService'
-
-export interface OrderProps {
-  data: string
-  delivery: boolean
-  total: number
-  payment: 'DEBIT' | 'CREDIT' | 'PIX' | 'MONEY' | 'DUPLICATE'
-  draft: boolean
-  address?: AddressProps
-}
 
 export class CreateOrderService {
   async execute(client_id: string) {
