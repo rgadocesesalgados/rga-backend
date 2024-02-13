2af4caaa97561434dc1c121fcf37d4981d4c7cf2
devHonorio <honorio.dev@gmail.com>
2024-02-13 15:45:13 -0300

    Verifica se o nome da categoria já existe antes de salvar

--- src/services/category/EditCategoryService.ts
+++ src/services/category/EditCategoryService.ts
@@ -2,6 +2,15 @@ import { prismaClient } from '../../prisma'
 
 export class EditCategoryService {
   async execute(name: string, new_name: string) {
+    const existCategory = await prismaClient.category.findFirst({
+      where: {
+        name: new_name,
+      },
+    })
+
+    if (existCategory) {
+      throw new Error('Essa categoria já existe.')
+    }
     const category = await prismaClient.category.update({
       where: {
         name,
