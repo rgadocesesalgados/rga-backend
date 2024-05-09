4388195cf3a1eb601a5f73d8efbab7f5bbf55ced
devHonorio <honorio.dev@gmail.com>
2024-05-09 15:46:36 -0300

    up

--- src/services/product/RemoveProductService.ts
+++ src/services/product/RemoveProductService.ts
@@ -2,6 +2,18 @@ import { prismaClient } from '../../prisma'
 
 export class RemoveProductService {
   async execute(id: string) {
+    console.log({ id })
+
+    const existProductInOrder = await prismaClient.orderProduct.findFirst({
+      where: {
+        product_id: id,
+      },
+    })
+
+    if (existProductInOrder) {
+      throw new Error('Esse produto contém pedidos')
+    }
+
     const product = await prismaClient.product.delete({
       where: {
         id,
