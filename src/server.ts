01fe97b279e4da63fe6b99870826ed8187f07445
devHonorio <honorio.dev@gmail.com>
2024-05-21 22:15:04 -0300

    cors

--- src/server.ts
+++ src/server.ts
@@ -5,7 +5,7 @@ import { routes } from './routes'
 
 const app = express()
 
-app.use(cors({ origin: process.env.URL_CORS }))
+app.use(cors({ origin: [process.env.URL_CORS] }))
 
 app.use(express.json())
 
