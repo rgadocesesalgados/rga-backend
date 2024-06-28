5534930fb1176d5003952e990c7ed9b37ce28d6f
devHonorio <honorio.dev@gmail.com>
2024-06-28 15:20:31 -0300

    adiciona funcionalidade para escolher para qual status mudar

--- src/controllers/relatorios/ToggleStatusController.ts
+++ src/controllers/relatorios/ToggleStatusController.ts
@@ -1,17 +1,21 @@
 import { Response } from 'express'
 import { RequestWithUser } from '../../middlewares/isAuthenticated'
 import { ToggleStatusService } from '../../services/relatorios/ToggleStatusService'
+import { OrderCreate } from '../../types/order'
 
 export class ToggleStatusController {
   async handle(req: RequestWithUser, res: Response) {
-    const { ids } = req.body as { ids: string[] }
+    const { ids, status } = req.body as {
+      ids: string[]
+      status: OrderCreate['status']
+    }
 
     if (!ids?.length) throw new Error('Id é obrigatório')
 
     const toggleStatusService = new ToggleStatusService()
 
-    const status = await toggleStatusService.execute(ids)
+    const response = await toggleStatusService.execute(ids, status)
 
-    return res.json(status)
+    return res.json(response)
   }
 }
--- src/services/relatorios/ToggleStatusService.ts
+++ src/services/relatorios/ToggleStatusService.ts
@@ -1,10 +1,11 @@
 import { prismaClient } from '../../prisma'
+import { OrderCreate } from '../../types/order'
 
 export class ToggleStatusService {
-  async execute(ids: string[]) {
+  async execute(ids: string[], status: OrderCreate['status'] = 'EM_PRODUCAO') {
     await prismaClient.order.updateMany({
       where: { id: { in: ids } },
-      data: { status: 'EM_PRODUCAO' },
+      data: { status },
     })
   }
 }
