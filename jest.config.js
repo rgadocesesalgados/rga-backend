5df10addce7c47084dd15252084c3f972f74e05f
José Honorio de Oliveira Sobrinho Neto <honorio.dev@gmail.com>
2024-10-07 14:38:35 -0300

    add option `--runInBand` and `testTimeout` to 10000

--- jest.config.js
+++ jest.config.js
@@ -1,7 +1,12 @@
+const expand = require('dotenv-expand')
+const dotenv = require('dotenv')
+expand.expand(dotenv.config({ path: '.env.development' }))
+
 /** @type {import('ts-jest').JestConfigWithTsJest} **/
 module.exports = {
   testEnvironment: 'node',
   transform: {
     '^.+.tsx?$': ['ts-jest', {}],
   },
+  testTimeout: 10000,
 }
