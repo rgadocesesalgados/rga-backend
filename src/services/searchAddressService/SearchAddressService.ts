b4691321e7960801730ca308f75f34d014df3bd0
José Honorio de Oliveira Sobrinho Neto <honorio.dev@gmail.com>
2024-11-26 22:23:49 -0300

    feat: add properts `address_complete` `frete_carro` and `frete_moto`

--- src/services/searchAddressService/SearchAddressService.ts
+++ src/services/searchAddressService/SearchAddressService.ts
@@ -3,7 +3,12 @@ import { prismaClient } from '../../prisma'
 export class SearchAddressService {
   async execute(query: string) {
     const address = await prismaClient.address.findMany({
-      select: { id: true, address_complete: true },
+      select: {
+        id: true,
+        address_complete: true,
+        frete_carro: true,
+        frete_moto: true,
+      },
       take: 10,
       where: {
         OR: [
